067cf71fedcc646946d06eb3d1a5a0f6268b3892
Kiran Shila <me@kiranshila.com>
2023-02-05 10:51:51 -0800

    very small buffer

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use thingbuf::{mpsc::with_recycle, Recycle};
 const UDP_PAYLOAD: usize = 8200;
 const WARMUP_PACKETS: usize = 1_000_000;
 const BACKLOG_BUFFER_PAYLOADS: usize = 4096;
-const BLOCK_PAYLOAD_POW: u32 = 13;
+const BLOCK_PAYLOAD_POW: u32 = 9;
 const BLOCK_PAYLOADS: usize = 2usize.pow(BLOCK_PAYLOAD_POW);
 const BLOCKS_TO_SORT: usize = 512;
 
