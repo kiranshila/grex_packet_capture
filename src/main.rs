d48c6f652093876ba5c339db88a1fff3e7010b7a
Kiran Shila <me@kiranshila.com>
2023-02-05 12:12:29 -0800

    tokio tuning

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use anyhow::bail;
 use core_affinity::CoreId;
 use std::time::{Duration, Instant};
 use thingbuf::{mpsc::with_recycle, Recycle};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::runtime::Builder;
 
 const UDP_PAYLOAD: usize = 8200;
 const WARMUP_PACKETS: usize = 1_000_000;
@@ -42,137 +44,145 @@ impl Recycle<PayloadBlock> for PayloadRecycle {
     }
 }
 
-#[tokio::main(flavor = "current_thread")]
-async fn main() -> anyhow::Result<()> {
-    // Setup the monitoring
-    console_subscriber::init();
+fn main() -> anyhow::Result<()> {
+    // Create the runtime
+    let rt = Builder::new_multi_thread()
+        .thread_stack_size(256 * 1024 * 1024)
+        .max_blocking_threads(4)
+        .build()?;
 
-    // Bind this thread to a core that shares a NUMA node with the NIC
-    if !core_affinity::set_for_current(CoreId { id: 8 }) {
-        bail!("Couldn't set core affinity");
-    }
+    // Spawn the root task
+    rt.block_on(async {
+        // Setup the monitoring
+        console_subscriber::init();
 
-    // Create the socket
-    let mut cap = Capture::new(60000)?;
-
-    // Create the channel to bench the copies
-    let (s, r) = with_recycle(RING_BLOCKS, PayloadRecycle::new());
-
-    // Preallocate the buffer with non-uninit values
-    for _ in 0..RING_BLOCKS {
-        s.send_ref().await?;
-        r.recv_ref().await;
-    }
-
-    // Sneaky bit manipulation (all bits to 1 to set that the index corresponding with *that bit* needs to be filled)
-    let mut to_fill = BLOCK_PAYLOADS - 1;
-
-    // Spawn a task to "sink" the payloads
-    tokio::spawn(async move {
-        while let Some(block) = r.recv_ref().await {
-            let mut big_sum = 0.0;
-            for payload in block.0 {
-                big_sum += payload.iter().fold(0.0, |x, b| x + *b as f32);
-            }
-            println!("{}", big_sum);
+        // Bind this thread to a core that shares a NUMA node with the NIC
+        if !core_affinity::set_for_current(CoreId { id: 8 }) {
+            bail!("Couldn't set core affinity");
         }
-    });
 
-    // "Warm up" by capturing a ton of packets
-    for _ in 0..WARMUP_PACKETS {
-        cap.capture().await?;
-    }
+        // Create the socket
+        let mut cap = Capture::new(60000)?;
 
-    let mut first_payload = true;
-    let mut oldest_count = 0;
-    let mut drops = 0;
-    let mut processed = 0;
+        // Create the channel to bench the copies
+        let (s, r) = with_recycle(RING_BLOCKS, PayloadRecycle::new());
 
-    // Sort N blocks, printing dropped packets
-    for _ in 0..BLOCKS_TO_SORT {
-        // First block to grab a reference to the next slot in the queue
-        let mut slot = s.send_ref().await.unwrap();
+        // Preallocate the buffer with non-uninit values
+        for _ in 0..RING_BLOCKS {
+            s.send_ref().await?;
+            r.recv_ref().await;
+        }
 
-        // Create a timer for average block processing
-        let mut time = Duration::default();
+        // Sneaky bit manipulation (all bits to 1 to set that the index corresponding with *that bit* needs to be filled)
+        let mut to_fill = BLOCK_PAYLOADS - 1;
 
-        for _ in 0..slot.0.len() {
-            // ----- CAPTURE
+        // Spawn a task to "sink" the payloads
+        tokio::spawn(async move {
+            while let Some(block) = r.recv_ref().await {
+                let mut big_sum = 0.0;
+                for payload in block.0 {
+                    big_sum += payload.iter().fold(0.0, |x, b| x + *b as f32);
+                }
+                println!("{}", big_sum);
+            }
+        });
 
-            // Capture an arbitrary payload
+        // "Warm up" by capturing a ton of packets
+        for _ in 0..WARMUP_PACKETS {
             cap.capture().await?;
-
-            // Time starts now to benchmark processing perf
-            let now = Instant::now();
-
-            // Decode its count
-            let count = count(&cap.buffer);
-            if first_payload {
-                oldest_count = count;
-                first_payload = false;
-            }
-
-            // ----- SORT
-
-            // Find its position in this block
-            if count < oldest_count {
-                // Drop this payload, it happened in the past
-                drops += 1;
-            } else if count >= oldest_count + slot.0.len() as u64 {
-                // Packet is destined for the future, insert into reorder buf
-                cap.backlog.insert(count, cap.buffer);
-            } else {
-                let idx = (count - oldest_count) as usize;
-                // Remove this idx from the `to_fill` entry
-                to_fill &= !(1 << idx);
-                // Packet is for this block! Insert into it's position
-                slot.0[idx] = cap.buffer;
-                processed += 1;
-            }
-
-            // Stop the timer and add to the block time
-            time += now.elapsed();
         }
-        // Now we'll fill in gaps with past data, if we have it
-        // Otherwise replace with zeros and increment the drop count
-        let block_process = Instant::now();
 
-        for (idx, buf) in slot.0.iter_mut().enumerate() {
-            // Check if this bit needs to be filled
-            if (to_fill >> idx) & 1 == 1 {
-                // Then either fill with data from the past, or set it as default
-                let count = idx as u64 + oldest_count;
-                if let Some(pl) = cap.backlog.remove(&count) {
-                    buf.clone_from_slice(&pl);
-                    processed += 1;
-                } else {
-                    let mut pl = [0u8; UDP_PAYLOAD];
-                    (pl[0..8]).clone_from_slice(&count.to_be_bytes());
-                    buf.clone_from_slice(&pl);
+        let mut first_payload = true;
+        let mut oldest_count = 0;
+        let mut drops = 0;
+        let mut processed = 0;
+
+        // Sort N blocks, printing dropped packets
+        for _ in 0..BLOCKS_TO_SORT {
+            // First block to grab a reference to the next slot in the queue
+            let mut slot = s.send_ref().await.unwrap();
+
+            // Create a timer for average block processing
+            let mut time = Duration::default();
+
+            for _ in 0..slot.0.len() {
+                // ----- CAPTURE
+
+                // Capture an arbitrary payload
+                cap.capture().await?;
+
+                // Time starts now to benchmark processing perf
+                let now = Instant::now();
+
+                // Decode its count
+                let count = count(&cap.buffer);
+                if first_payload {
+                    oldest_count = count;
+                    first_payload = false;
+                }
+
+                // ----- SORT
+
+                // Find its position in this block
+                if count < oldest_count {
+                    // Drop this payload, it happened in the past
                     drops += 1;
+                } else if count >= oldest_count + slot.0.len() as u64 {
+                    // Packet is destined for the future, insert into reorder buf
+                    cap.backlog.insert(count, cap.buffer);
+                } else {
+                    let idx = (count - oldest_count) as usize;
+                    // Remove this idx from the `to_fill` entry
+                    to_fill &= !(1 << idx);
+                    // Packet is for this block! Insert into it's position
+                    slot.0[idx] = cap.buffer;
+                    processed += 1;
+                }
+
+                // Stop the timer and add to the block time
+                time += now.elapsed();
+            }
+            // Now we'll fill in gaps with past data, if we have it
+            // Otherwise replace with zeros and increment the drop count
+            let block_process = Instant::now();
+
+            for (idx, buf) in slot.0.iter_mut().enumerate() {
+                // Check if this bit needs to be filled
+                if (to_fill >> idx) & 1 == 1 {
+                    // Then either fill with data from the past, or set it as default
+                    let count = idx as u64 + oldest_count;
+                    if let Some(pl) = cap.backlog.remove(&count) {
+                        buf.clone_from_slice(&pl);
+                        processed += 1;
+                    } else {
+                        let mut pl = [0u8; UDP_PAYLOAD];
+                        (pl[0..8]).clone_from_slice(&count.to_be_bytes());
+                        buf.clone_from_slice(&pl);
+                        drops += 1;
+                    }
                 }
             }
+
+            // Then reset to_fill
+            to_fill = BLOCK_PAYLOADS - 1;
+            // Move the oldest count forward by the block size
+            oldest_count += slot.0.len() as u64;
+            let block_process_time = block_process.elapsed();
+            // At this point, we'd send the "sorted" block to the next stage by dropping slot
+            // Print timing info
+            println!(
+                "Processing - {} us per packet\tBlock - {} us - Backlog {}",
+                time.as_micros() as f32 / BLOCK_PAYLOADS as f32,
+                block_process_time.as_micros(),
+                cap.backlog.len()
+            );
         }
 
-        // Then reset to_fill
-        to_fill = BLOCK_PAYLOADS - 1;
-        // Move the oldest count forward by the block size
-        oldest_count += slot.0.len() as u64;
-        let block_process_time = block_process.elapsed();
-        // At this point, we'd send the "sorted" block to the next stage by dropping slot
-        // Print timing info
+        println!("Dropped {drops} packets while processing {processed} packets.");
         println!(
-            "Processing - {} us per packet\tBlock - {} us - Backlog {}",
-            time.as_micros() as f32 / BLOCK_PAYLOADS as f32,
-            block_process_time.as_micros(),
-            cap.backlog.len()
+            "That's a drop rate of {}%",
+            100.0 * drops as f32 / (drops + processed) as f32
         );
-    }
-
-    println!("Dropped {drops} packets while processing {processed} packets.");
-    println!(
-        "That's a drop rate of {}%",
-        100.0 * drops as f32 / (drops + processed) as f32
-    );
-    Ok(())
+        Ok(())
+    })
 }
